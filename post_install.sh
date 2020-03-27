3c5d55e3873176edddddf5bff962a3c408df1f6f
colemamd <mdcoleman001@gmail.com>
2020-03-27 17:15:19 -0400

    elevate more priviliges

--- post_install.sh
+++ post_install.sh
@@ -2,8 +2,8 @@
 
 
 cd /tmp && curl -s https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep "AdguardHome_freebsd_amd64.tar.gz" | cut -d : -f 2,3 | tr -d \" | wget -qi -
-tar -xvzf AdGuardHome*.tar.gz -C /root
-rm AdGuardHome*.tar.gz
+sudo tar -xvzf AdGuardHome*.tar.gz -C /root
+sudo rm AdGuardHome*.tar.gz
 
 sudo chmod +x /usr/local/etc/rc.d/adguard
 sudo sysrc 'adguard_enable=YES'
