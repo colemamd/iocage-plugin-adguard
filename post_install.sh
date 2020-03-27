3b9d59e4f8ee05a2383253e74b3492f3b01b33b4
colemamd <mdcoleman001@gmail.com>
2020-03-27 17:17:37 -0400

    remove evelated privileges

--- post_install.sh
+++ post_install.sh
@@ -2,11 +2,11 @@
 
 
 cd /tmp && curl -s https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep "AdguardHome_freebsd_amd64.tar.gz" | cut -d : -f 2,3 | tr -d \" | wget -qi -
-sudo tar -xvzf AdGuardHome*.tar.gz -C /root
-sudo rm AdGuardHome*.tar.gz
+tar -xvzf AdGuardHome*.tar.gz -C /root
+rm AdGuardHome*.tar.gz
 
-sudo chmod +x /usr/local/etc/rc.d/adguard
-sudo sysrc 'adguard_enable=YES'
+chmod +x /usr/local/etc/rc.d/adguard
+sysrc 'adguard_enable=YES'
 service adguard start
 
 echo "AdGuard Home is now installed" > /root/PLUGIN_INFO
